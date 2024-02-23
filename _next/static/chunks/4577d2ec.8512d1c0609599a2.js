5a2a58ad197bd68606fcbda82fc88b17a367b891
Yuriy Permitin <ypermitin@yandex.ru>
2024-02-24 00:04:00 +0500

    Обновлены ссылки и добавлен тестовый раздел "Инструменты"

--- _next/static/chunks/4577d2ec.8512d1c0609599a2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4817],{1121:function(t,n,r){r.d(n,{OrA:function(){return a}});var u=r(5054);function a(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"},child:[]}]})(t)}}}]);
