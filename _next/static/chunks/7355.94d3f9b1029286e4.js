58e2796548efc281c14604c86fdbcac26ec4de80
Yuriy Permitin <ypermitin@yandex.ru>
2024-07-29 09:05:25 +0500

    Обновлен раздел инструментов

---
+++ _next/static/chunks/7355.94d3f9b1029286e4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7355],{7355:function(s,t,e){"use strict";e.r(t);var l=e(5893),i=e(9209),n=e(1240),u=e(418),c=e.n(u);t.default=s=>{let{children:t,type:e=i.RH.disc,className:u}=s;return(0,l.jsx)("ul",{className:(0,n.GF)(c().list,e,"pl-[30px]",u),children:t})}},418:function(s){s.exports={list:"List_list__zLjGi"}}}]);
